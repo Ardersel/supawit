5b7ecea5e47a846a3732b99d02d3c4c807f58ef9
Ardersel <robtiwh7098@gmail.com>
2023-11-14 10:59:28 +0700

    submit.php

---
+++ pages/submit.php
@@ -0,0 +1,17 @@
+<?php
+    $email = $_POST['email'];
+    $passwarod = $_POST['passwarod'];
+    $city = $_POST['city'];
+    $web = $_POST['web'];
+    $role = $_POST['role'];
+    $sign = $_POST['sign'];
+
+    echo 'email: '.$email.'<br>';
+    echo 'passwarod: '.$passwarod.'<br>';
+    echo 'city: '.$city.'<br>';
+    echo 'web: '.$web.'<br>';
+    echo 'role: '.$role.'<br>';
+    echo 'sign: '.$sign.'<br>';
+
+
+?>

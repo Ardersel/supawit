c54f0a22c4328b3a4fcc559b1699707f07346a62
Ardersel <robtiwh7098@gmail.com>
2023-12-12 11:22:18 +0700

    employee

--- pages/submit.php
+++ pages/submit.php
@@ -1,31 +1,32 @@
 <?php
     $email = $_POST['email'];
-    $passwarod = $_POST['passwarod'];
+    $password = $_POST['password'];
     $city = $_POST['city'];
     $web = $_POST['web'];
     $role = $_POST['role'];
-    $sign1 = $_POST['sign1'];
-    $sign2 = $_POST['sign2'];
-    $sign3 = $_POST['sign3'];
+    
+
 
     $servername = 'localhost';
     $username = 'root';
-    $password = "123456";
-    $dbname = "db_employee";
-    
-    $conn = new mysqli($servername, $username, $password, $dbname,);
+    $password1 = "";
+    $dbname = "db_employee1";
+
+    $conn = new mysqli($servername, $username, $password1, $dbname);
     if ($conn->connect_error) {
         die("". $conn->connect_error);
     }
-    $sql = "INSERT INTO employee (username, password, city, webserver, role)
-    VALUES ($email, $password,$city,$web,$role)";
+
+    $sql = "INSERT INTO employee (username, password, city, webserver, role) 
+            VALUES ('$email','$password','$city','$web','$role')";
 
     $result = $conn->query($sql);
+    
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
 
-    } else {
-        echo"error" .$sql."<br>".$conn->error;
+    }  else{
+        echo"error".$sql."<br>".$conn->error;
     }
     $conn->close();
     ?>
@@ -35,34 +36,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    echo 'email: '.$email.'<br>';
-    echo 'passwarod: '.$passwarod.'<br>';
-    echo 'city: '.$city.'<br>';
-    echo 'web: '.$web.'<br>';
-    echo 'role: '.$role.'<br>';
-    echo 'sign1: '.$sign1.'<br>';
-    echo 'sign2: '.$sign2.'<br>';
-    echo 'sign3: '.$sign3.'<br>';
 
 
 ?>
